0bac1c5daf21d46317bb49e5ced1412b7289c716
thirukos <thirukos@tcd.ie>
2023-01-26 17:28:25 +0000

    project-update

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,8 +1,10 @@
 const express=require("express")
 const router=express.Router()
 
-router.get("/hello",(req,res)=>{
-    res.status(200).send({message:"Hello thirukos! :)"})
-})
+// router.get("/hello",(req,res)=>{
+//     res.status(200).send({message:"Hello thirukos! :)"})
+// })
+
+
 
 module.exports=router;
