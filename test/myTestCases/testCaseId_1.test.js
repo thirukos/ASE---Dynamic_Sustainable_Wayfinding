ac62a969225d53f9c7ef6a0d513904ebf0ba3a91
thirukos <thirukos@tcd.ie>
2023-03-02 16:22:54 +0000

    030223.1622

---
+++ test/myTestCases/testCaseId_1.test.js
@@ -0,0 +1,33 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import axios from 'axios';
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+
+describe('Registration process', () => {
+    it('should return a 201 status code and a success message after registering new user', (done) => {
+        const user = {
+            username: 'testuser',
+            email: 'testuser@gmail.com',
+            password: 'password@123'
+        };
+
+        chai.request('http://localhost:3000')
+            .post('/api/register')
+            .send({
+                username: 'testuser',
+                email: 'testuser@gmail.com',
+                password: 'password@123'
+            })
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res).to.have.status(201);
+                // expect(res.body.success).to.be.true;
+                // expect(res.body.message).to.equal("User Register Successfully");
+                done();
+            });
+
+    });
+});
